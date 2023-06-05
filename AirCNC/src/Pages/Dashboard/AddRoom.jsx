9600426eee9bd6916709177b18a03f4d96e034b9
Shuyibe48 <gthrough48@gmail.com>
2023-06-06 02:32:23 +0600

    add room form implement

---
+++ AirCNC/src/Pages/Dashboard/AddRoom.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddRoomForm from '../../components/Forms/AddRoomForm';
+
+const AddRoom = () => {
+    return (
+        <AddRoomForm />
+    );
+};
+
+export default AddRoom;
