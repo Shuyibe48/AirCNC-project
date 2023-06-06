d15e824aa77ff762b268272808d14526a5ee36bb
Shuyibe48 <gthrough48@gmail.com>
2023-06-06 11:14:17 +0600

    asdfd

--- AirCNC/src/Pages/Dashboard/AddRoom.jsx
+++ AirCNC/src/Pages/Dashboard/AddRoom.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddRoomForm from '../../components/Forms/AddRoomForm';
 
 const AddRoom = () => {
+    const [loading, setLoading] = useState(false)
+    const [uploadButtonText, setUploadButtonText] = useState('Upload Image')
+
+    const handleSubmit = event => {
+        event.preventDefault
+        setLoading(true)
+
+        const location = event.target.location.value
+        const title = event.target.title.value
+        const from = dates.startDate
+        const to = dates.endDate
+        const price = event.target.price.value
+        const guests = event.target.total_guest.value
+        const bedrooms = event.target.bedrooms.value
+        const bathrooms = event.target.bathrooms.value
+        const description = event.target.description.value
+        const category = event.target.category.value
+        const image = event.target.image.files[0]
+
+
+        console.log('hello from form');
+    }
     return (
-        <AddRoomForm />
+        <AddRoomForm handleSubmit={handleSubmit} loading={loading} />
     );
 };
 
--- AirCNC/src/components/Forms/AddRoomForm.jsx
+++ AirCNC/src/components/Forms/AddRoomForm.jsx
@@ -6,13 +6,13 @@ const AddRoomForm = ({
     handleSubmit,
     dates,
     handleDates,
-    loading = false,
+    loading,
     handleImageChange,
     uploadButtonText,
 }) => {
     return (
         <div className='w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
                     <div className='space-y-6'>
                         <div className='space-y-1 text-sm'>
